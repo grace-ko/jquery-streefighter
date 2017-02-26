41251bdcf54e4e6c38af14e7e6bad16a1838f781
grace-ko <gracekoder@gmail.com>
2017-02-25 21:13:23 -0800

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+#Street Fighter jQuery Exercise 
+###Ryu gets ready on mouse hover, throws a hadouken when clicked, and poses when x is pressed 
+![screenshot](screenshot.png)
--- js/app.js
+++ js/app.js
@@ -1,29 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $("#intro-music").get(0).play();
-    $('.ryu').mouseenter(function(){
-        $('.ryu-still').hide();
-        $('.ryu-ready').show();
-    })
-    .mouseleave(function() {
-        $('.ryu-still').show();
-        $('.ryu-ready').hide();
-    })
-    .mousedown(function(){
-        playHadouken();
-        $('.ryu-ready').hide();
-        $('.ryu-throwing').show();
-        $('.hadouken').finish().show().animate(
-            {'left': '1020px'},
-            500,
-            function(){
-                $('.hadouken').hide();
-                $('.hadouken').css('left', '520px');
-            });
-    })
-    .mouseup(function(){
-        $('.ryu-throwing').hide();
-        $('.ryu-ready').show();
-    });
+    $('.ryu').mouseenter(function() {
+            $('.ryu-still').hide();
+            $('.ryu-ready').show();
+        })
+        .mouseleave(function() {
+            $('.ryu-still').show();
+            $('.ryu-ready').hide();
+        })
+        .mousedown(function() {
+            playHadouken();
+            $('.ryu-ready').hide();
+            $('.ryu-throwing').show();
+            $('.hadouken').finish().show().animate({ 'left': '1020px' },
+                500,
+                function() {
+                    $('.hadouken').hide();
+                    $('.hadouken').css('left', '520px');
+                });
+        })
+        .mouseup(function() {
+            $('.ryu-throwing').hide();
+            $('.ryu-ready').show();
+        });
 
 });
 
@@ -34,16 +33,14 @@ function playHadouken() {
 }
 $(document).keydown(function(e) {
     if (e.keyCode == 88) {
-      $('.ryu-ready').hide();
-      $('.ryu-still').hide();
-      $('.ryu-cool').show();
+        $('.ryu-ready').hide();
+        $('.ryu-still').hide();
+        $('.ryu-cool').show();
     }
 }).keyup(function(e) {
     if (e.keyCode == 88) {
-      $('.ryu-cool').hide();
-      $('.ryu-still').show();
-      $('.ryu-ready').show();
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+        $('.ryu-ready').show();
     }
-  });
-
-
+});
